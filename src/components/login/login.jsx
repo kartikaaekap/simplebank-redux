668031bab2c26048e9958b2cd7ca83c4900ae1f2
Dian Rahmaji <dianrahmaji@gmail.com>
2020-10-03 22:43:20 +0700

    redux config

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,45 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import loginImg from "../../assets/login.svg";
 import { withRouter } from "react-router-dom";
 
-class Login extends React.Component {
-  constructor(props) {
-    super(props);
+const Login = () => {
+  const [login, setLogin] = useState({});
 
-    this.openTransactionPage = this.openTransactionPage.bind(this)
-  }
+  const openTransactionPage = () => {
+    // this.props.history.push("/transactions");
+    setLogin({
+      message: "Hello",
+    });
+  };
 
-  openTransactionPage() {
-    this.props.history.push('/transactions')
-  }
-
-  render() {
-    return (
-      <div className="base-container" ref={this.props.containerRef}>
-        <div className="header">Login</div>
-        <div className="content">
-          <div className="image">
-            <img src={loginImg} alt="login" />
-          </div>
-          <div className="form">
-            <div className="form-group">
-              <label htmlFor="username">Username</label>
-              <input type="text" name="username" placeholder="username" />
-            </div>
-            <div className="form-group">
-              <label htmlFor="password">Password</label>
-              <input type="password" name="password" placeholder="password" />
-            </div>
-          </div>
+  return (
+    <div className="base-container">
+      <div className="header">Login</div>
+      <div className="content">
+        <div className="image">
+          <img src={loginImg} alt="login" />
         </div>
-        <div className="footer">
-          <button type="button" className="btn" onClick={this.openTransactionPage}>
-            {/* <Link to="/transactions">Login</Link> */} Login
-          </button>
+        <div className="form">
+          <div className="form-group">
+            <label htmlFor="username">Username</label>
+            <input type="text" name="username" placeholder="username" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input type="password" name="password" placeholder="password" />
+          </div>
         </div>
       </div>
-    );
-  }
-}
+      <div className="footer">
+        <button type="button" className="btn" onClick={openTransactionPage}>
+          {/* <Link to="/transactions">Login</Link> */} Login
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default withRouter(Login)
+export default withRouter(Login);
